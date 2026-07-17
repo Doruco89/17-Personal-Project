bb6e437e311fe99d65e1e0db1d58c1646afde57e
Doruco89 <ryu4746@gmail.com>
2026-07-17 10:23:56 +0900

    HW EV4

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 # 오피넷(한국석유공사) 오픈API 인증키
 # 발급: https://www.opinet.co.kr/user/custapi/openApiNew.do
-OPINET_API_KEY = os.environ.get("IwN3xscffjGk8uvjZWPCtyMFd7SMpHuizCVkPEE02Q", "")
+OPINET_API_KEY = os.environ.get("OPINET_API_KEY", "")
 
 # 공공데이터포털 - 한국환경공단_전기자동차 충전소 정보 서비스키
 # 발급: https://www.data.go.kr/data/15076352/openapi.do (활용신청 -> 자동승인)
-EV_API_KEY = os.environ.get("8cb337c7869cd0fca833b6b0e7d0a5b6c42ed84dddd05a28eee2718bd4d07fb8", "")
+EV_API_KEY = os.environ.get("EV_API_KEY", "")
 
 # 키가 없으면 데모(샘플) 데이터로 동작
 OIL_DEMO_MODE = not bool(OPINET_API_KEY)
